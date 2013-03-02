dafc1838b05dd64b1a7b611707d59ea080e68c6b
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-02 18:52:05 +0100

    Modified package name

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,7 +4,7 @@
 #
 #  include realvnc::client
 class realvnc::client {
-  package { 'RealVNC-Viewer-5.0.4.dmg':
+  package { 'RealVNC-Viewer-5.0.4':
     provider => 'appdmg',
     source   => 'https://yum.cegeka.be/osx_packages/RealVNC-Viewer-5.0.4.dmg',
   }
